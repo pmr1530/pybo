fc9c66860c3992401e41f90597853cc184d14f2b
pyun <pmr1530@naver.com>
2021-06-09 16:33:21 +0900

    2-06 clear

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
 
 
 def index(request):
@@ -19,6 +19,12 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
-
+def answer_create(request, question_id):
+    """
+    답변 등록
+    """
+    question=get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
+    return redirect('pybo:detail',question_id=question.id)
 
 # Create your views here.
