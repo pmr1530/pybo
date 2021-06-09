79e190e41a3c92418b9d4bd97c0e2ddf2338b969
pyun <pmr1530@naver.com>
2021-06-09 13:38:56 +0900

    2-03 clear

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Question
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields=['subject','content']
+    list_display=['id','subject','short_content','create_date']
+    def short_content(self,item):
+        return item.content[:10]
+
+
 # Register your models here.
+
+# admin.site.register(Question, QuestionAdmin)
+
