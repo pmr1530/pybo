1c118ca6bc8e47bd349127eff6fd27617aeb97d8
pyun <pmr1530@naver.com>
2021-06-09 10:50:59 +0900

    2-02 migrate

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+#-------------------------------edit----------------#
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+def __str__(self):
+    return self.subject
+
+#---------------------------------------------------#
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+def __str__(self):
+    return self.subject
+
+#---------------------------------------------------#
+
 # Create your models here.
