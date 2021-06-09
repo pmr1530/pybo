223c5fdfeee85bbc915016072a869822bff6f8fa
pyun <pmr1530@naver.com>
2021-06-09 15:04:17 +0900

    2-04 clear

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("hello world")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question,pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+
 # Create your views here.
