eaebfb2c12294b2f9ff382f7382219877e8e7657
pyun <pmr1530@naver.com>
2021-06-10 11:37:19 +0900

    2-10(1)

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta :
         model=Question
-        fields=['subject', 'content']    
+        fields=['subject', 'content']
+        labels = {
+            'subject' : '제목',
+            'content' : '내용',
+        }
+        # widget 항목 삭제
+
--- pybo/views.py
+++ pybo/views.py
@@ -7,8 +7,18 @@ def question_create(request):
     """
     질문 등록
     """
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form':form})
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+        
+    else:
+        form = QuestionForm()
+    context = { 'form' : form }
+    return render(request, 'pybo/question_form.html', context)
 
 def index(request):
     """
