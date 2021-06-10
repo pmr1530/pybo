644cd9bcf1aaef82205a2479d915606217043144
pyun <pmr1530@naver.com>
2021-06-10 16:19:12 +0900

    3-02 clear

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+
 
 def question_create(request):
     """
@@ -24,8 +26,17 @@ def index(request):
     """
     pybo 목록 출력
     """
+    # 입력 인자
+    page = request.GET.get('page','1') # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    #페이징 처리
+    paginator = Paginator(question_list, 10) #페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
